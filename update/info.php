9a899c13eb972f2c2af1cf41b8a9e6db702cfe34
Majid <majidabedini11@gmail.com>
2025-05-11 15:20:25 +0330

    Update info.php
    info fully modified

--- update/info.php
+++ update/info.php
@@ -2,30 +2,29 @@
 // The output of this file must be a JSON that returns the new version information
 header('Content-Type: application/json');
 
-$plugin = isset($_GET['theme']) ? $_GET['theme'] : '';
+$theme = isset($_GET['theme']) ? $_GET['theme'] : '';
 
-if ($plugin === 'hello-elementor-child/functions.php') {
+if ($theme === 'hello-elementor-child/functions.php') {
     $info = [
-        'name' => 'CONTENT GENERATOR',
-        'version' => '1.1.1',
-        'author' => 'Majid',
+        'name' => 'hello-elementor-child',
+        'version' => '1.0.5',
+        'author' => 'AradBranding',
         'requires' => '5.0',
         'tested' => '6.4',
-        'downloaded' => 1000,
         'last_updated' => date('Y-m-d'),
-        'description' => 'Generates content using CHAT-GPT.',
-        'changelog' => '<h4>نسخه 1.1.1</h4>
+        'description' => 'A custom theme powered by GPT integration.',
+        'changelog' => '<h4>نسخه 1.0.5</h4>
         <ul>
-            <li>بهبود عملکرد و حل مشکلات</li>
-            <li>اضافه شدن قابلیت جدید</li>
-            <li>رفع باگ‌های گزارش شده</li>
+            <li>بهبود عملکرد قالب</li>
+            <li>سازگاری با نسخه‌های جدید وردپرس</li>
+            <li>رفع اشکالات گزارش‌شده</li>
         </ul>',
-        'download_url' => 'https://github.com/alfred1997/hello-elementor-child.git'
+        'download_url' => 'https://example.com/themes/my-custom-theme-1.0.5.zip'
     ];
     
     echo json_encode($info);
 } else {
     http_response_code(404);
-    echo json_encode(['error' => 'theme not found']);
+    echo json_encode(['error' => 'Theme not found']);
 }
-?> 
+?>
